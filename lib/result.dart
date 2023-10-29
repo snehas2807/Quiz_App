6ca3f377a0181ad9a852193d52907685d8ed0be9
Sneha Kumari <110496760+snehas2807@users.noreply.github.com>
2023-10-29 12:20:26 +0530

    Update result.dart

--- lib/result.dart
+++ lib/result.dart
@@ -1,31 +1,51 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  // const Result({super.key});
   final int resultScore;
+  final VoidCallback resethandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resethandler);
 
   String get resultPhrase {
-    String resultText;
-    if (resultScore <= 8) {
-      resultText = 'You are awesome and inncoent!';
-    } else if (resultScore <= 12) {
-      resultText = 'Preety likeable!';
-    } else if (resultScore <= 16) {
-      resultText = 'You are .... strange!?';
-    } else {
-      resultText = 'You are so bad!';
-    }
+    var resultText;
+
+    if (resultScore > 30)
+      resultText = 'Congragulation!!!! Clown Outfit Unlocked';
+    else
+      resultText = 'Abhi bhi time hai nikal le yaha se';
     return resultText;
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          OutlinedButton(
+            onPressed: resethandler,
+            child: Text('Restart the quiz'),
+            // style: ButtonStyle(backgroundColor: Color.blue),
+            //child: Column (  // we use this by using shortcut key ctrl+k and ctrl+s and then wrap column
+            //children: <widget>[
+            //Text( resultPhrase,
+            //style:TextStyle(fontSize: 36, fontWeight: FontWeight: FontWeight.bold),
+            //textAlign: TextAlign.center,),
+            //FlatButton(child:Text('Restart Quiz!'),
+            //textxoolor: Colors.blue,
+            //onPressed:resetHandler,),],),
+          )
+        ],
       ),
     );
   }
