a1a377a377b6ca28de2b24fbb98dd8e4bf409ce3
Sneha Kumari <110496760+snehas2807@users.noreply.github.com>
2023-10-29 12:18:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,118 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/quiz.dart';
+import 'result.dart';
+// import 'package:first_app/questions.dart';
 
-import './quiz.dart';
-import './result.dart';
-// void main(){
-// // runApp(My App());
+// void main()
+// {
+//   runApp(MyApp());
+
 // }
-
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); //uper wale shorthand hai bass
 
 class MyApp extends StatefulWidget {
+  // var question = [
+  //   {
+  //     'questionText': 'What is your name',
+  //     'answer': ['Akshat', 'Yash', 'Harsh', 'Shahvez'],
+  //   },
+  //   {
+  //     'questionText': 'What is your favorite color',
+  //     'answer': ['Blue', 'black', 'Green', 'yelow'],
+  //   },
+  // ];
   @override
   State<StatefulWidget> createState() {
-    //TODO: implement createState
-    return _MyAppState();
+    // TODO: implement createState
+    return _MyAppState(); //ye baad wali class to return kar rha hai, MyAppState ko connect kiya isse
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  final List<Map<String, Object>> _questions = const [
+class _MyAppState extends State<
+    MyApp> //<MyApp> dono classes to apse connect karne ke liye likhA hai
+//_MyAppState me '_' add kiya taki koi aur file MyAppState use na kar sake varna wierd bugs aa skte hai
+{
+  var question = [
     {
-      'questionText': 'What\'s your favourite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 20},
-        {'text': 'blue', 'score': 30},
-        {'text': 'Green', 'score': 40},
+      'questionText': 'Why you choose HIT ?',
+      'answer': [
+        {'text': 'Option bhi kya tha ', 'score': 10},
+        {'text': 'HIT provide overall development', 'score': 0},
+        {'text': 'Kuch to lena hi tha na', 'score': 0},
+        {'text': 'HIT is one of the top college in India', 'score': 0}
       ],
     },
     {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': [
-        {'text': 'Cow', 'score': 10},
-        {'text': 'Bull', 'score': 20},
-        {'text': 'Ghoda', 'score': 30},
-        {'text': 'Gadha', 'score': 40},
+      'questionText': 'Which is top college in westBengal ?',
+      'answer': [
+        {'text': 'Jadavpur', 'score': 10},
+        {'text': 'HIT', 'score': 0},
+        {'text': 'IEM', 'score': 0},
+        {'text': 'BC ROY', 'score': 0}
       ],
     },
     {
-      'questionText': 'What\'s your favourite Fruit?',
-      'answers': [
-        {'text': 'Apple', 'score': 10},
-        {'text': 'Banana', 'score': 20},
-        {'text': 'Mango', 'score': 30},
-        {'text': 'Strawberry', 'score': 40},
+      'questionText': 'Where HIT college of engineering is located ?',
+      'answer': [
+        {'text': 'Haldia', 'score': 10},
+        {'text': 'Kolkata', 'score': 0},
+        {'text': 'Delhi', 'score': 0},
+        {'text': 'Ranchi', 'score': 0}
+      ],
+    },
+    {
+      'questionText': 'Who is the HOD of CSE-CS ?',
+      'answer': [
+        {'text': 'PJ', 'score': 0},
+        {'text': 'AS', 'score': 0},
+        {'text': 'SSM', 'score': 0},
+        {'text': 'SS', 'score': 10}
       ],
     },
   ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
+  var questionIndex = 0;
+  var totalscore = 0;
+
+  void resetQuiz() {
+    setState(() {
+      questionIndex = 0;
+      totalscore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
-    //var aBool = true;
-    //aBool = false;
-// +=
-    _totalScore = _totalScore + score;
+    totalscore = totalscore + score;
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      //SetState logic statement ko change karne ke liye sue kiya jata, isske andart bass logic stmt daalana hai
+      questionIndex = questionIndex + 1;
     });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
+
+    print(questionIndex);
+    if (questionIndex < question.length) {
+      print('We have another question!');
     } else {
-      print('No More Questions!');
+      print('No more questions!!');
     }
   }
-  //single = means you are storing a value and == means you are comapring the values
 
-  @override
-  Widget build(BuildContext) {
+  @override //overrride just makes the code cleaner, wo dikhta hai ki humne stateless widget ko override karke apna Widget bnaya joki waise bhi bnaya tha
+  Widget build(BuildContext context) {
+    // var question = [
+    //   'What is your name',
+    //   'whta is your age',
+    // ];
+    // questions =[]; //does not work if questions is a const
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Quiz App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
-              )
-            : Result(_totalScore),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('My first App'),
+            ),
+            body: questionIndex < question.length
+                ? Quiz(
+                    question: question,
+                    answerQuestion: _answerQuestion,
+                    questionIndex: questionIndex)
+                : Result(totalscore, resetQuiz)));
   }
 }
